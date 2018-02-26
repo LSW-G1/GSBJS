a51492296a1ebe348828fa2d549978d9e9142baf
Kevin <ticubius@gmail.com>
2018-02-26 10:50:57 +0100

    GSBJS - DEV.1.00 - First Commit
    - INIT REPO

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+    <title>Ma première Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css"/>
+
+</head>
+<body>
+<div data-role="page">
+    <div data-role="header">
+        <h1>Un Titre</h1>
+    </div>
+    <div data-role="content">
+        <p>Bonjour le monde !</p>
+    </div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</body>
+</html>
