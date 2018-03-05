bf3772cb4c6a1a03447dbb7137dbeed1918b45da
ktartiere <ktartiere@gmail.com>
2018-03-05 10:44:22 +0100

    GSBJS - V.1.00 - TP1 - I'm DONE
    - TP1: Completed.
    Wasn't fun. 0/10.

--- js/monJs.js
+++ js/monJs.js
@@ -1,31 +1,46 @@
-$(function ()
-{
-	$('#pageconnexion #btnconnexion').bind("click", function (e)
-	{
-		e.preventDefault();
-		var mdp = $("#pageconnexion #mdp").val();
-		var login = $("#pageconnexion #login").val();
-		$.post("ajax/traiterconnexion.php",
-			{
-				"mdp": mdp,
-				"login": login
-			},
-			foncRetourConnexion, "json");
-	});
+$(function () {
+    $("#panel #ajouterunrapport").bind("click", function (e) {
+        e.preventDefault();
+        $.mobile.changePage("#pageajouterunrapport");
+    });
 
-	function foncRetourConnexion(data)
-	{
-		if (data != null)
-		{
-			$.mobile.changePage("#pageaccueil");
-		}
-		else
-		{
-			$("#pageconnexion #message").css(
-			{
-				color: 'red'
-			});
-			$("#pageconnexion #message").html("erreur de login et/ou mdp");
-		}
-	}
+    $("#panel #modifierunrapport").bind("click", function (e) {
+        e.preventDefault();
+        $.mobile.changePage("#pagechoisirrapportamodifier");
+    });
+
+    $("#footer #mesMedecins").bind("click", function (e) {
+        e.preventDefault();
+        $.mobile.changePage("#pagemedecins");
+    })
+
+    $("#footer #mesVisites").bind("click", function (e) {
+        e.preventDefault();
+        $.mobile.changePage("#pagevisites");
+    })
+
+    $('#pageconnexion #btnconnexion').bind("click", function (e) {
+        e.preventDefault();
+        var mdp = $("#pageconnexion #mdp").val();
+        var login = $("#pageconnexion #login").val();
+        $.post("ajax/traiterconnexion.php",
+            {
+                "mdp": mdp,
+                "login": login
+            },
+            foncRetourConnexion, "json");
+    });
+
+    function foncRetourConnexion(data) {
+        if (data != null) {
+            $.mobile.changePage("#pageaccueil");
+        }
+        else {
+            $("#pageconnexion #message").css(
+                {
+                    color: 'red'
+                });
+            $("#pageconnexion #message").html("erreur de login et/ou mdp");
+        }
+    }
 });
--- vues/pagemedecins.php
+++ vues/pagemedecins.php
@@ -1,7 +1,12 @@
-<div data-role="page" id="pagevisites">
-    <?php include "entetepagevisites.html"; ?>
+<div data-role="page" id="pagemedecins">
+    <?php
+    include "vues/entetepage.html"; //inclue l'entete
+    ?>
+
     <div data-role="content">
-        <?php include "logo.html"; ?>
+        <p>Vous êtes sur la page médecins</p>
     </div>
-    <?php include "piedpage.html"; ?>
+    <?php
+    include "vues/piedpage.html";
+    ?>
 </div>
